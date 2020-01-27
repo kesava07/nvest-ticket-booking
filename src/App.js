5bd9f38036ca3717c892d1849776a17642f64760
kesava07 <kesava07.ip@gmail.com>
2020-01-27 03:07:59 -0800

    task completion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,137 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import KeyBoard from './Components/KeyBoard';
+import AutoGenWheel from './Components/AutoGenWheel';
+import Tickets from './Components/Tickets';
 
 function App() {
+  const [ticketNumber, setTicketNumber] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [tickets, setTickets] = React.useState([]);
+  const [error, setError] = React.useState("");
+
+  // Entering the 6 digit ticket number
+  const enterNumber = (e) => {
+    const copyNumbers = [...ticketNumber];
+    copyNumbers.push(e.target.innerHTML);
+    setTicketNumber(copyNumbers);
+  }
+
+  // Removing the ticket number
+  const deleteTicketNumber = () => {
+    setTicketNumber([]);
+  };
+
+  // Handling back space button
+  const handleBack = () => {
+    const copyTicketNum = [...ticketNumber];
+    if (ticketNumber.length >= 0) {
+      copyTicketNum.pop();
+    }
+    setTicketNumber(copyTicketNum);
+  };
+
+  // Auto pin generation
+  const handleAutoGenNumber = () => {
+    setLoading(true);
+    setTimeout(() => {
+      const randomTicketNum = Math.floor(100000 + Math.random() * 900000);
+      setTicketNumber(randomTicketNum.toString().split(''))
+      setLoading(false);
+    }, 1000);
+  }
+
+  // Adding the ticket
+  const addTicket = () => {
+    setError('');
+    if (ticketNumber.length !== 6) {
+      sendError("Please enter 6 digits ticket number");
+      return
+    } else if (ticketNumber.length > 6) {
+      sendError("Ticket number should be 6 digits only");
+    } else if (tickets.length >= 5) {
+      sendError("A person can book only 5 tickets");
+    } else if (chekForDuplicate(tickets)) {
+      // Checking for duplicate tickets
+      sendError("The ticket number is already exists");
+      return;
+    } else {
+      const generateTicket = {
+        id: tickets.length + 1,
+        ticketNumber: ticketNumber.join(''),
+        date: Date.now()
+      }
+      const copyTickets = [...tickets];
+      copyTickets.push(generateTicket)
+      setTickets(copyTickets);
+      setTicketNumber([]);
+    }
+  }
+  const chekForDuplicate = tickets => {
+    if (tickets.length) {
+      const duplicateTicket = tickets.filter(ticket => ticket.ticketNumber === ticketNumber.join(''));
+      if (duplicateTicket.length) {
+        return true
+      } else return false;
+    } else {
+      return false
+    }
+  }
+
+  // Deleting the ticket
+  const handleDeleteTicket = index => {
+    const copyTickets = [...tickets];
+    copyTickets.splice(index, 1);
+    setTickets(copyTickets);
+  };
+
+  // Dismissing the errors
+  const dismissError = () => setError('');
+
+  const sendError = (error) => {
+    setError(error);
+    setTimeout(() => {
+      setError("");
+    }, 1500)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid App_container">
+      {error && (
+        <div className="alert alert-danger alert-position">
+          <strong>Error!</strong> {error}
+          <button type="button" className="close" onClick={dismissError}>&times;</button>
+        </div>
+      )}
+      <h3 className="text-center text-white pt-4">Create A Ticket</h3>
+      <div className="container inner_container">
+        <div className="row">
+          <div className="col-lg-6 p-3">
+            {/* key board component */}
+            <KeyBoard
+              ticketNumber={ticketNumber}
+              addTicket={addTicket}
+              enterNumber={enterNumber}
+              deleteTicketNumber={deleteTicketNumber}
+              handleBack={handleBack}
+            />
+          </div>
+          <div className="col-lg-6 p-3">
+            {/* Auto ticket number generation component */}
+            <AutoGenWheel
+              handleAutoGenNumber={handleAutoGenNumber}
+              loading={loading}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="mt-3">
+        {/* Displaying ticket numbers */}
+        <Tickets
+          tickets={tickets}
+          handleDeleteTicket={handleDeleteTicket}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/Components/KeyBoard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const KeyBoard = (props) => {
+    const { ticketNumber, addTicket, enterNumber, deleteTicketNumber, handleBack } = props;
+    const [keyDigits] = React.useState([7, 8, 9, 4, 5, 6, 1, 2, 3])
+    return (
+        <div>
+            <div className="card card-body">
+                <div className="ticket_number">
+                    {ticketNumber.length === 0 && <span className="danger-badge">Enter 6 digits</span>}
+                    {ticketNumber && ticketNumber.map((num, i) => (
+                        <span key={i} className="d-inline">{num}</span>
+                    ))}
+                </div>
+                <div className="board-row">
+                    {keyDigits.map(digit => (
+                        <button key={digit} disabled={ticketNumber.length >= 6} className="square-button" onClick={enterNumber}>
+                            {digit}
+                        </button>
+                    ))}
+                </div>
+                <div className="board-row">
+                    <button className="square-button" onClick={handleBack} disabled={ticketNumber.length === 0}>
+                        <i className="fa fa-long-arrow-left"></i>
+                    </button>
+                    <button className="square-button" onClick={enterNumber} disabled={ticketNumber.length >= 6} >0</button>
+                    <button className="square-button" onClick={deleteTicketNumber} disabled={ticketNumber.length === 0}>
+                        <i className="fa fa-trash text-danger"></i>
+                    </button>
+                </div>
+                <button className="btn btn-secondary" onClick={addTicket}>
+                    <i className="fa fa-plus-square"></i> Add Ticket
+              </button>
+            </div>
+        </div>
+    )
+}
+
+export default KeyBoard;
